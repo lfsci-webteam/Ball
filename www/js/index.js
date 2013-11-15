d89b523055bba50814510dd6fe8794569a8db1fe
lfsci-webteam <lfsci-webteam@byu.edu>
2013-11-15 10:27:56 -0700

    Animation V1

--- www/js/index.js
+++ www/js/index.js
@@ -3,11 +3,36 @@ var records = new Array();
 var width = screen.width
 var height = screen.height
 
+var x = 0;
+var y = 0;
+
 var debugMode = true;
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
+var angle = 0;
+var x = 0;
+var y = 0;
+var w = (window.innerWidth - 50) / 2;
+var h = (window.innerHeight - 50) / 2;
+
+function ballCircle() {
+	debug("ballCircle");
+	x = w + w * Math.cos(angle * Math.PI / 180);
+	y = h + h * Math.sin(angle * Math.PI / 180);
+
+	$("#ball").css("left", x + 'px');
+	$("#ball").css("top", y + 'px');
+
+	angle++;
+	if (angle > 360) {
+		angle = 0;
+	}
+	setTimeout(ballCircle, 20);
+}
+
 function onDeviceReady() {
+	ballCircle();
 }
 
 $(document).ready(function () {
